8dd22593e69cc33fa37e5369bfdd06f47925b5bb
Pulasthi Lenaduwa <69018605+plen276@users.noreply.github.com>
2025-09-06 11:06:11 +1200

    Add led flash when processing commands (#5)

--- picow-python/led.py
+++ picow-python/led.py
@@ -1,4 +1,5 @@
 from machine import Pin
+from utime import sleep
 
 from config import LED_PIN
 
@@ -21,3 +22,28 @@ def led_off():
     """Turn LED off"""
     print("LED Off")
     led.off()
+
+
+def blink():
+    """Blink LED"""
+    led_on()
+    sleep(0.1)
+    led_off()
+
+
+def flash_on():
+    """Flash LED on"""
+    for _ in range(2):
+        led_on()
+        sleep(0.1)
+        led_off()
+    led_on()
+
+
+def flash_off():
+    """Flash LED off"""
+    for _ in range(2):
+        led_off()
+        sleep(0.1)
+        led_on()
+    led_off()
